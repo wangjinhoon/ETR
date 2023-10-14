3c0ad6cee746d39cca6acd72c20f5f44d5493373
wangjinhoon <83506637+wangjinhoon@users.noreply.github.com>
2023-10-14 13:29:58 +0900

    Update ConsoleApplication4.cpp

--- ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,43 +1,57 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <Open3D/Open3D.h>
 #include<queue>
 using namespace std;
 
-void PaintMesh(open3d::geometry::TriangleMesh& mesh,
-    const Eigen::Vector3d& color) {
-    mesh.vertex_colors_.resize(mesh.vertices_.size());
-    for (size_t i = 0; i < mesh.vertices_.size(); i++) {
-        mesh.vertex_colors_[i] = color;
-    }
-}
-
 int main() {
     // Define input and output paths
     using namespace open3d;
-    std::string input_path = "C:\\Users\\jh813\\source\\repos\\ConsoleApplication4\\test.ply";
+    std::string input_path = "C:\\Users\\jh813\\source\\repos\\ConsoleApplication4\\cloud5.ply";
     std::string output_file_path = "C:\\Users\\jh813\\test.obj";
 
 
-
-    auto cloud_ptr = io::CreatePointCloudFromFile(input_path);
     // Load point cloud
-    //visualization::DrawGeometries({ cloud_ptr }, "TestPCDFileFormat", 1920, 1080);
+    auto cloud_ptr = io::CreatePointCloudFromFile(input_path);
+    //visualization::DrawGeometries({ cloud_ptr }, "TestPCDFileFormat", 1920, 1080); // point cloud data viewer
 
-    //// Perform Poisson surface reconstruction
-    int depth = 10;
+    // Filter PointCloud data to eliminate points where nx, ny, and nz are all zeroes.
+    open3d::geometry::PointCloud pcd_filtered;
+
+    for (int i = 0; i < cloud_ptr->points_.size(); i++) {
+        Eigen::Vector3d normal = cloud_ptr->normals_[i];
+        if (normal.norm() > 0.0) {
+            pcd_filtered.points_.push_back(cloud_ptr->points_[i]);  //copy location information
+            pcd_filtered.normals_.push_back(normal);
+            pcd_filtered.colors_.push_back(cloud_ptr->colors_[i]);  // Copy color information
+        }
+    }
+    
+    pcd_filtered.EstimateNormals();
+
+   open3d::io::WritePointCloud("cleaned_point_cloud.ply", pcd_filtered);
+
+    // Perform Poisson surface reconstruction
+    int depth = 8;
     float width = 0.0f;
     float scale = 1.1f;
     bool linear_fit = false;
-    auto result = open3d::geometry::TriangleMesh::CreateFromPointCloudPoisson(*cloud_ptr, depth, width, scale, linear_fit);
-
+    /*std::tuple<std::shared_ptr<TriangleMesh>, std::vector<double>>
+      TriangleMesh::CreateFromPointCloudPoisson(const PointCloud& pcd,
+                                          size_t depth,
+                                          float width,
+                                          float scale,
+                                          bool linear_fit,
+                                          int n_threads)*/
+    auto result = open3d::geometry::TriangleMesh::CreateFromPointCloudPoisson(pcd_filtered, depth, width, scale, linear_fit);
+    
+    //Output is shared_ptr<TriangleMesh>, std::vector<double>, so only shared_ptr information is extracted as a get function
     auto mesh_ptr = std::get<std::shared_ptr<open3d::geometry::TriangleMesh>>(result);
 
-    // Create a vector of shared pointers to geometries
 
-
-    if (io::WriteTriangleMesh(output_file_path, *mesh_ptr)) {
-        std::cout << "Mesh saved to " << output_file_path << std::endl;
+    //save masy to path 
+    if (io::WriteTriangleMesh(output_file_path, *mesh_ptr)) { 
+        std::cout << "Mesh saved to " << output_file_path << std::endl; 
     }
 
     return 0;
